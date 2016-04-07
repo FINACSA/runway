639669d191ccda67517d81b8058d8ca242534beb
Jorge Pardinas <jparbros@gmail.com>
2016-04-07 09:30:03 -0500

    config redis

--- sidekiq.rb
+++ sidekiq.rb
@@ -5,10 +5,15 @@ require './lib/worker'
 
 Sidekiq.configure_server do |config|
   config.average_scheduled_poll_interval = 15
+  config.redis = { url: 'redis://localhost:6379/12' }
 
   config.on(:startup) do
     Sidekiq.schedule = YAML.load_file(File.expand_path("../config/scheduler.yml",__FILE__))
     Sidekiq::Scheduler.enabled = true
     Sidekiq::Scheduler.reload_schedule!
   end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/12' }
 end
